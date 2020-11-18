0de9fd94f78c539d6d3eeff5777ca8a5237d6da5
sgadde <gaddesurendrakumar@gmail.com>
2020-11-18 13:55:07 +0530

    Modifed to add new methods

--- jpqlandnativesql/src/main/java/com/surendra/springdata/jpqlandnativesql/repos/StudentRepository.java
+++ jpqlandnativesql/src/main/java/com/surendra/springdata/jpqlandnativesql/repos/StudentRepository.java
@@ -2,6 +2,7 @@ package com.surendra.springdata.jpqlandnativesql.repos;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -16,5 +17,18 @@ public interface StudentRepository extends CrudRepository<Student, Long> {
 	List<Object[]> findAllStudentsPartialData() ;
 	@Query("from Student where firstName=:firstName")
 	List<Student> findAllStudentsWithFirstName(@Param("firstName") String firstName) ;
+
+	@Query("from Student where score>:min and score<:max")
+	List<Student> findStudentsForGivenScores(@Param("min") int min, @Param("max") int max);
+
+	@Modifying
+	@Query("delete from Student where firstName = :firstName")
+	void deleteStudentsByFirstName(@Param("firstName") String firstName);
+
+	@Query(value = "select * from student", nativeQuery = true)
+	List<Student> findAllStudentNQ();
+
+	@Query(value = "select * from student where fname=:firstName", nativeQuery = true)
+	List<Student> findByFirstNQ(@Param("firstName")String firstName);
 
 }
